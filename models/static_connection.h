46a0633a3a406a64a635a4a69b979d267c86777d
Itaru Kitayama <itaru.kitayama@riken.jp>
2020-08-30 08:33:53 +0900

    Remove an incorrect comment from send_non_virtual() function.

--- models/static_connection.h
+++ models/static_connection.h
@@ -157,7 +157,7 @@ public:
     e.set_delay_steps( get_delay_steps() );
     e.set_receiver( *get_target( tid ) );
     e.set_rport( get_rport() );
-    //e(); // exceptions can not be thrown on the target
+    e();
   }
 
   void
